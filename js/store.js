cfe1541cc9886285a7c68a97da2afbcba384413a
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-03-29 01:06:51 +0330

    23

--- js/store.js
+++ js/store.js
@@ -34,47 +34,47 @@ function showCart(cartf){
     let div1 = document.createElement('div');
     div1.classList.add('cart-row');
 
-  let div2 = document.createElement('div');
-  div2.classList = 'cart-item cart-column';
+    let div2 = document.createElement('div');
+    div2.classList = 'cart-item cart-column';
 
-  let img = document.createElement('img');
-  img.classList.add('cart-item-image');
-  img.src = row.srcImg;
-  
-  let span = document.createElement('span');
-  span.classList.add('cart-item-title');
-  span.innerHTML = row.title;
+    let img = document.createElement('img');
+    img.classList.add('cart-item-image');
+    img.src = row.srcImg;
+    
+    let span = document.createElement('span');
+    span.classList.add('cart-item-title');
+    span.innerHTML = row.title;
 
-  div2.append(img, span);
+    div2.append(img, span);
 
-  let span2 = document.createElement('span');
-  span2.classList = 'cart-price cart-column';
-  span2.innerHTML = `$${row.price}`;
+    let span2 = document.createElement('span');
+    span2.classList = 'cart-price cart-column';
+    span2.innerHTML = `$${row.price}`;
 
-  let div3 = document.createElement('div');
-  div3.classList = 'cart-quantity cart-column';
+    let div3 = document.createElement('div');
+    div3.classList = 'cart-quantity cart-column';
 
-  let input = document.createElement('input');
-  input.classList = 'cart-quantity-input'
-  input.setAttribute('type', 'number');
-  input.value = row.quantity;
-  input.addEventListener('change',function(){
-    setQuntity(input.value, row.id, cart);
-  })
+    let input = document.createElement('input');
+    input.classList = 'cart-quantity-input'
+    input.setAttribute('type', 'number');
+    input.value = row.quantity;
+    input.addEventListener('change',function(){
+      setQuntity(input.value, row.id, cart);
+    })
 
-  let removeBtn = document.createElement('button');
-  removeBtn.classList = 'btn btn-danger';
-  removeBtn.innerHTML = 'REMOVE';
-  removeBtn.addEventListener('click',function(){
-    cart = cartf.filter(item=>{return item.id !== row.id })
-    console.log(cart);
-    showCart(cart);
-  })
+    let removeBtn = document.createElement('button');
+    removeBtn.classList = 'btn btn-danger';
+    removeBtn.innerHTML = 'REMOVE';
+    removeBtn.addEventListener('click',function(){
+      cart = cartf.filter(item=>{return item.id !== row.id })
+      console.log(cart);
+      showCart(cart);
+    })
 
-  div3.append(input, removeBtn);
+    div3.append(input, removeBtn);
 
-  div1.append(div2, span2, div3)
-   cartItems.append(div1);
+    div1.append(div2, span2, div3)
+    cartItems.append(div1);
   })
 
 }
